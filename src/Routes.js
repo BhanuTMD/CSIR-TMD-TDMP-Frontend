c4463628863ba32b28f3c0eb643d705f2256e695
riteshece <riteshgupta.14august@gmail.com>
2025-04-04 15:29:10 +0530

    changes file name in all sections
    changes file name in all sections

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./Components/auth/Login";
 import Signup from "./Components/auth/Signup";
-import Section1 from "./Components/Section1";
+import SectionOne from "./Components/SectionOne";
 import TechSearch from "./Components/TechSearch";
-import Section2 from "./Components/Section2";
-import Section3 from "./Components/Section3";
-import Section4 from "./Components/Section4";
+import SectionTwo from "./Components/SectionTwo";
+import SectionThree from "./Components/SectionThree";
+import SectionFour from "./Components/SectionFour";
 import ChangePassword from "./Components/auth/ChangePassword";
 import WelcomePage from "./Components/WelcomePage";
 import OTPVerify from "./Components/auth/OtpVerify";
@@ -15,8 +15,7 @@ import OTPLoginVerify from "./Components/auth/OtpLoginVerify";
 // import Search from "./Components/Search";
 // import JsonDisplay from "./Components/JsonDisplay";
 // import Search from "./Components/Search";
-// import SearchTable from "./Components/SearchTable";
-
+import SearchTable from "./Components/SearchTable";
 
 const AppRoutes = () => {
   return (
@@ -25,11 +24,11 @@ const AppRoutes = () => {
         <Route index element={<Login />} />
         <Route path="welcomePage" element={<WelcomePage />} />
         <Route path="signup" element={<Signup />} />
-        <Route path="section1" element={<Section1 />} />
+        <Route path="section1" element={<SectionOne />} />
         <Route path="techSearch" element={<TechSearch />} />
-        <Route path="section2" element={<Section2 />} />
-        <Route path="section3" element={<Section3 />} />
-        <Route path="section4" element={<Section4 />} />
+        <Route path="section2" element={<SectionTwo />} />
+        <Route path="section3" element={<SectionThree />} />
+        <Route path="section4" element={<SectionFour />} />
         <Route path="changePassword" element={<ChangePassword />} />
         <Route path="otpVerify" element={<OTPVerify />} />
         <Route path="otpLoginVerify" element={<OTPLoginVerify />} />
@@ -37,6 +36,7 @@ const AppRoutes = () => {
         {/* <Route path="Search" element={<Search />} />
         <Route path="JsonDisplay" element={<JsonDisplay />} /> */}
         <Route path="TechSearch" element={<TechSearch />} />
+        <Route path="SearchTable" element={<SearchTable />} />
       </Route>
     </Routes>
   );
